280c11387d4bf080543c504e490e9c7a574336ca
Cha Haneum <developer_haneum_11@icloud.com>
2022-08-13 14:58:23 +0900

    temp

--- app.js
+++ app.js
@@ -1,96 +1,104 @@
-const react = () => {
-  const render = f => {
-    f();
-  };
-  const options = {
-    currentStateKey: 0,
-    states: [],
-  };
-  function useState(initState) {
-    if (options.states.length === options.currentStateKey)
-      options.states.push(initState);
+let React = (function () {
+  let global = {}; // define a global variable where we store information about the component
+  let index = 0; // index to keep track of the component's state
+  function render(Component) {
+    global.Component = Component;
+    const instance = Component(); // get the instance of the component
+    index = 0;
+    instance.render(); // call the component's render function
 
-    const state = options.states[options.currentStateKey];
-    const setState = newState => {
-      options.states[options.currentStateKey] = newState;
-      render();
-    };
-    options.currentStateKey += 1;
-    return [state, setState];
+    global.instance = instance; // store the component's instance for any future calls of the component's functions
+    return global; // return the global variable
   }
-  return [render, useState];
-};
 
-const root = document.querySelector('#root');
-const app = () => {
-  const render = react();
-  render = () => {
-    home(); // 기억해야 한다!!!!
+  function useState(initialState) {
+    if (!global) {
+      throw new Error('Need a global');
+    }
+
+    if (!global.hooks) {
+      global.hooks = []; // this array holds the state of the component
+    }
+
+    const hooks = global.hooks;
+    const currentState = global.hooks[index] || initialState;
+    hooks[index] = currentState; // memoize the state for future access
+    firstrender = true;
+
+    const setState = (function () {
+      let currentIndex = index; // copy the index so each useState call will have it's own "closed" value over index (currentIndex)
+      return function (value) {
+        global.hooks[currentIndex] = value;
+        render(global.Component); //re-render the component after state change
+      };
+    })();
+    index = index + 1;
+    return [currentState, setState];
+  }
+
+  function useEffect(cb, deps) {
+    const hooks = global.hooks;
+    // getting older dependencies from the hooks array since
+    // we are storing dependencies as a sub-array inside the hooks array
+    let oldDeps = hooks[index];
+
+    // if no dependencies are provided,
+    // the callback function will be called at each re-render
+    let hasChanged = true;
+
+    if (oldDeps) {
+      // checking if the old dependencies are different from older dependencies
+      hasChanged = deps.some((d, index) => !Object.is(d, oldDeps[index]));
+    }
+    if (hasChanged) {
+      console.log(hooks[index], cb);
+      cb();
+    } // if dependencies has changed call the callback function.
+
+    hooks[index] = deps; //store dependencies inside the hooks array as a sub-array
+    index++; // increment index for any other useEffect calls
+  }
+
+  return { render, useState, useEffect };
+})();
+
+function Component() {
+  // Component is called at each re-render. index is reset to 0.
+
+  const [count, setCount] = React.useState(0);
+  // hooks: [0], currentIndex: 0,  Incremented Index: 1
+
+  const [word, setWord] = React.useState('');
+  // hooks: [0, ''], currentIndex: 1,  Incremented Index: 2
+
+  const countSetter = () => {
+    setCount(count + 1);
   };
-};
-app();
 
-const home = () => {
-  const [state, setState] = useState(1); //  그래야 이게 작동함;
-  root.innerHTML = `
-    <ul>
-      ${state.map(item => `<li>${item}</li>`).join('')}
-    </ul>
-    <button id="append">Append item</button>
-    <ul>
-      ${value.map(item => `<li>${item}</li>`).join('')}
-    </ul>
-    <button id="append2">Append item</button>
-  `;
-};
+  const wordSetter = word => {
+    setWord(word);
+  };
 
-// const options = {
-//   currentStateKey: 0,
-//   states: [],
-// };
+  function render() {
+    console.log(`Count is: ${count}, Word is: ${word}`);
+  }
 
-// const react = () => {
-//   function useState(initState) {
-//     if (options.states.length === options.currentStateKey)
-//       options.states.push(initState);
+  React.useEffect(() => {
+    console.log('hookssss!!!!');
+  }, [count, word]);
+  // hooks: [0, '', [0, '']], currentIndex: 2,  Incremented Index: 3
 
-//     const state = options.states[options.currentStateKey];
-//     const setState = newState => {
-//       options.states[options.currentStateKey] = newState;
-//       render();
-//     };
-//     options.currentStateKey += 1;
-//     return [state, setState];
-//   }
-//   return useState;
-// };
+  React.useEffect(() => {
+    console.log('hooks2!!!!!');
+  }, []);
+  // hooks: [0, '', [0, ''], [] ], currentIndex: 3,  Incremented Index: 4
 
-// // 이중 선언 왜 오류?
-// const useState = react();
-// const root = document.querySelector('#root');
-// const app = () => {
-//   // let state = ['item1', 'item2', 'item3', 'item4'];
-//   const render = () => {
-//     const [state, setState] = useState(['item1'], render);
-//     const [value, setValue] = useState([1], render);
-//     console.log(state, value);
-//     root.innerHTML = `
-//       <ul>
-//         ${state.map(item => `<li>${item}</li>`).join('')}
-//       </ul>
-//       <button id="append">Append item</button>
-//       <ul>
-//         ${value.map(item => `<li>${item}</li>`).join('')}
-//       </ul>
-//       <button id="append2">Append item</button>
-//     `;
-//     document.querySelector('#append').addEventListener('click', e => {
-//       setState([...state, `item${state.length + 1}`]);
-//     });
-//     document.querySelector('#append2').addEventListener('click', () => {
-//       setValue([...value, [value.length + 1]]);
-//     });
-//   };
-//   render();
-// };
-// app();
+  return { render, countSetter, wordSetter };
+}
+
+const global = React.render(Component); // hooks: [ 0, '', [ 0, '' ], [] ]
+global.instance.countSetter(); // hooks: [ 1, '', [ 1, '' ], [] ]
+global.instance.countSetter(); // hooks: hooks: [ 2, '', [ 2, '' ], [] ]
+global.instance.countSetter(); // hooks: [ 3, '', [ 3, '' ], [] ]
+global.instance.wordSetter('yooo'); // hooks: [ 3, 'yooo', [ 3, 'yooo' ], [] ]
+global.instance.wordSetter('ssup'); // hooks: [ 3, 'yooo', [ 3, 'yooo' ], [] ]
